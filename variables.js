f8b7551f5bcd9fb44e333e8cf772086aebe6b4d7
Lalit Pagare <lalitpagarerealme@gmail.com>
2024-06-05 16:47:23 +0530

    variables

---
+++ variables.js
@@ -0,0 +1,58 @@
+// Variables in javascript
+
+// old ES5
+
+// var a = 45;
+// var b = "name";
+// var c = 2.5;
+// var d = true;
+
+// console.log(a);
+// console.log(b);
+
+// console.log(c);
+// console.log(d);
+
+// ES6 new
+// let a = 5;
+// let b = "name";
+
+// console.log(a);
+// console.log(b);
+
+// const a = 5;
+// const b = "xyz";
+
+// console.log(a);
+// console.log(b);
+
+// a = 12;
+// b = "xyz";
+// console.log(a);
+// console.log(b);
+
+// var a = 5;
+
+// var a = 12;
+
+// var a = 10
+
+
+
+// let a = 12
+
+// // let a = 45
+
+// // let a = 56
+
+// a = 34
+// console.log(a);
+
+
+const PI = 3.14
+
+PI = 3.33
+
+console.log(PI)
+
+
